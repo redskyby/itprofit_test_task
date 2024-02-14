5b98c0cd792f6e018d19cd797e10dd0826fdf46d
Docenko Pasha <pashadocenko@gmail.com>
2024-02-14 22:42:41 +0300

    style loaders were added.

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -24,6 +24,17 @@ export default (env : EnvVariables) => {
         module : {
             rules :[
                 {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        "style-loader",
+                        // Translates CSS into CommonJS
+                        "css-loader",
+                        // Compiles Sass to CSS
+                        "sass-loader",
+                    ],
+                },
+                {
                     test : /\.tsx?$/,
                     use : 'ts-loader',
                     exclude : '/node_module',
